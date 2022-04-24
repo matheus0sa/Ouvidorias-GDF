c79a3e3409b2dbff5e304cd476acf1cb7a4fefd8
Matheus de Sá <matheusdesa55@gmail.com>
2022-04-24 20:38:36 -0300

    Conexão com MS SQL e para inserção dos dados

--- tratamento e carregamento.py
+++ tratamento e carregamento.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+import pyodbc
 import shutil
 import os
 from extracao import baixar_csv
-from datetime import datetime, date
+from datetime import datetime
 
 
 def converter_data(data):
@@ -13,7 +14,7 @@ def converter_data(data):
     return data
 
 
-hoje = pd.to_datetime(date(datetime.today()))
+hoje = pd.to_datetime(datetime.date(datetime.today()))
 
 df = pd.read_csv('dados.csv', sep='|')
 df['Abertura'] = df['Abertura'].apply(converter_data)
@@ -24,3 +25,22 @@ if df_hoje.shape[1] < 1:
     arquivo = baixar_csv()
     shutil.move("C:\\Users\\mathe\\Downloads\\" + arquivo, '.\\')
     os.rename(arquivo, 'dados.csv')
+
+server = 'LAPTOP-H9VSD33T\SQLEXPRESS'
+database = 'ouvidorias'
+cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};Server=localhost\SQLEXPRESS;Database=ouvidorias;Trusted_Connection=yes;')
+
+cursor = cnxn.cursor()
+
+cursor.execute('DELETE FROM [tb_ouvidorias]')
+for index, row in df.iterrows():
+     cursor.execute("INSERT INTO tb_ouvidorias ("
+                    "Abertura,Identificada,Situação,"
+                    "Classificação,Assunto,Região_Administrativa,"
+                    "Unidade,Orgao,Tipo_Entrada) "
+                    "values (?,?,?,?,?,?,?,?,?)"
+                    , row['Abertura'],row['Identificada'],row['Situação'],
+                    row['Classificação'],row['Assunto'],row['Região_Administrativa'],
+                    row['Unidade'],row['Orgao'],row['Tipo_Entrada'])
+cnxn.commit()
+cursor.close()
